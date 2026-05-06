a0d18df4e03c66bb6966ae1033e7c9429b53169b
summertimeWah <dtkiki6615@gmail.com>
2026-05-06 17:59:25 +0800

    fastapi

--- src/calculate.py
+++ src/calculate.py
@@ -1,2 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
 def add_func(a, b):
     return a + b
+
+@app.get("/")
+def home():
+    return {"status": "Online", "message": "簡易版計算機API"}
+
+@app.get("/add")
+def calculate_add(a: float, b:float):
+    result = add_func(a, b)
+    return {"operation": "addition", "a":a, "b":b, "result": result}
+
